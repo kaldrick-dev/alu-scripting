04c34d71608cb2cdf830b1c0c15d152b8e4a40bb
kaldrick-dev <a.mpanokal@alustudent.com>
2026-02-23 20:01:45 +0200

    Improved PEP8 Validation

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,14 +4,14 @@ import requests
 
 
 USER_AGENT = {
-    "User-Agent": "alu-api-advanced/1.0"
+    "User-Agent": "Mozilla/5.0 (compatible; alu-api-advanced/1.0)"
 }
 
 
 def top_ten(subreddit):
     """Print the first 10 hot post titles or None if invalid."""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    params = {"limit": 10}
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    params = {"limit": 10, "raw_json": 1}
 
     response = requests.get(
         url,
