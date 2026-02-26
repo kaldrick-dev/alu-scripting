64909420fc596afd1109d6c03fc361c1522f0ab4
kaldrick-dev <a.mpanokal@alustudent.com>
2026-02-26 15:09:07 +0200

    Fixed formatting

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -6,12 +6,14 @@ import requests
 
 def top_ten(subreddit):
     """Main function"""
-    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {"User-Agent": "python:top_ten:v1.0 (by /u/reddit_api_script)"}
 
-    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
-    try:
-        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
-        HOT_POSTS = RESPONSE.json().get("data").get("children")
-        [print(post.get('data').get('title')) for post in HOT_POSTS]
-    except Exception:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         print(None)
+        return
+
+    hot_posts = response.json().get("data", {}).get("children", [])
+    for post in hot_posts:
+        print(post.get("data", {}).get("title"))
