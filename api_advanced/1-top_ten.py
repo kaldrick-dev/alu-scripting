9143347fdf5bd1ce0e06d825e12c8919d2a210a3
kaldrick-dev <a.mpanokal@alustudent.com>
2026-03-31 13:45:58 +0200

    Fix

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,29 +1,16 @@
 #!/usr/bin/python3
-"""Reddit API module."""
-
+""" top_ten.py """
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the first 10 hot posts for a subreddit."""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {"User-Agent": "python:api_advanced:v1.0 (by /u/reddit_script)"}
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-    except requests.exceptions.RequestException:
-        print(None)
-        return
-
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         print(None)
         return
-
-    try:
-        posts = response.json().get("data", {}).get("children", [])
-    except ValueError:
-        print(None)
-        return
-
+    posts = response.json()['data']['children']
     for post in posts:
-        print(post.get("data", {}).get("title", ""))
+        print(post['data']['title'])
